ffa0cc476d58082cbb62d053844901070eef959b
IslamMohamedHassan <IslamMohamedHassan>
2023-05-31 00:14:26 +0300

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PartnersSlider from '../Components/Home Components/logoSlideShow/logoSlideShow';
+import Footer from '../Components/Footer/Footer';
+import SearchBar from '../Components/Home Components/Search/Search';
+import News from '../Components/Home Components/News/News';
+
+const Home = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Home;
+    
